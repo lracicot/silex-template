462d65624bd1ad8df4ba63a731f75ab6a855ef26
Louis Racicot <info@louisracicot.com>
2017-02-27 12:05:25 -0500

    add comments in app.php file

--- app/app.php
+++ app/app.php
@@ -4,25 +4,37 @@ define('BASEPATH', dirname(__DIR__));
 
 require_once BASEPATH.'/vendor/autoload.php';
 
+// Dotenv is a library that reads the `.env` file
+// to set environment variables. Those variables can
+// also be defined by the server.
 $dotenv = new Dotenv\Dotenv(BASEPATH);
 $dotenv->load();
 $dotenv->required('ENVIRONMENT')->allowedValues(['development', 'staging', 'testing', 'production']);
 $dotenv->required('BASEURI');
 
+// This is the Silex Application, basically, it is
+// a dependency injection container.
 $app = new Silex\Application();
 
+// We set the debug to true only if we are in dev mode
 if ($_ENV['ENVIRONMENT'] == 'development') {
     $app['debug'] = true;
 }
+
+// The asset service helps with the assets paths in the templates
+// It enables the {{ assets() }} twig function
 $app->register(new Silex\Provider\AssetServiceProvider(), [
   'assets.base_path' => $_ENV['BASEURI']
 ]);
 
+// This is the PSR-3 compliant logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), [
     'monolog.logfile' => BASEPATH.'/var/logs/'.$_ENV['ENVIRONMENT'].'.log',
     'monolog.level' => Monolog\Logger::DEBUG, // NOTICE, WARNING, ERROR
 ]);
 
+// The default template for the views is twig
+// It will autoload any twig files from the paths
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => [
         BASEPATH.'/app/Resources/views',
@@ -34,6 +46,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     ]
 ));
 
+// This is a helper function that returns the full url of the site
 $app['absolute_url'] = function () {
     $base_url = '';
     if (isset($_ENV['BASEURL'])) {
@@ -61,8 +74,12 @@ $app['absolute_url'] = function () {
 //     ],
 // ]);
 
+// We mount the default contorller.
+// You can add more controllers here
 $app->mount($_ENV['BASEURI'], new MyApp\DefaultController());
 
+// Start the app
 $app->run();
 
+// We returns the app because phpunit needs it
 return $app;
